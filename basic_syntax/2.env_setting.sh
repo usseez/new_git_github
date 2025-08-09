27d7dbf837b61e33be5635522dfff345dcd8d42d
abc <abc@naver.com>
2025-08-09 19:59:24 +0900

    fix

--- basic_syntax/2.env_setting.sh
+++ basic_syntax/2.env_setting.sh
@@ -10,3 +10,22 @@ git remote add origin 레포주소
 git remote remove origin
 # 원격지 주소 변경
 git remote set-url origin 레포주소
+# git 설정정보(config) 확인
+git config --list
+
+
+# 타인 레포 clone받는 방법 2가지
+# 1. commit이력 그대로 가져오기
+git clone 타인레포주소
+# 해당 폴더로 이동 후 github 레포주소 변경
+git remote set-url origin 나의 레포 주소
+git push origin main
+
+# 2. 커밋이력 없이 레포 가져오기
+git clone 타인레포주소
+# 해당 폴더 안으로 이동 후 .git폴더 삭제
+git init
+git remote add origin 나의 레포 주소
+git add .
+git commit -m "first"
+git push origin master 
